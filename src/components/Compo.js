ae94992844db6640b6f79d7fa0a807939085ca19
amypo-reactapp <reactapp@amypo.in>
2024-02-19 14:06:32 +0000

    User

---
+++ src/components/Compo.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {Component} from 'react';
+const Compo=()=>{
+    return(
+        <div>npm start
+            <p>hello</p>
+        </div>
+    )
+}
+export Compo;
